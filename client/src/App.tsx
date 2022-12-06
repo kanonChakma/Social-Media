45f639f654fd1a5da5d96c92c2913975ca294454
kanon chakma <kanonchakma@pop-os.localdomain>
2022-12-06 13:48:11 +0600

    feat: router set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/homePage/HomePage";
+import Login from "./pages/Login/Login";
+import ProfilePage from "./pages/profilePage/ProfilePage";
 
 const App: React.FC = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
