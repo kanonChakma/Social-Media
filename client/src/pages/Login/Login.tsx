fba51b6e1bf3e095d607ee317dd358703ed837c0
kanon chakma <kanonchakma@pop-os.localdomain>
2022-12-15 11:53:10 +0600

    feat: add form

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import LoginForm from "../../component/LoginForm";
+import RegisterForm from "../../component/RegisterForm";
 
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const [isLogin, setIsLogin] = useState<Boolean>(false);
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        sx={{ backgroundColor: `${theme.palette.background.alt}` }}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        sx={{ backgroundColor: `${theme.palette.background.alt}` }}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socipedia, the Social Media for Sociopaths!
+        </Typography>
+        {isLogin ? (
+          <LoginForm setIsLogin={setIsLogin} isLogin={isLogin} />
+        ) : (
+          <RegisterForm setIsLogin={setIsLogin} isLogin={isLogin} />
+        )}
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
