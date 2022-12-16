e29cbf9b2555a9a8ba13ee1ba95060bc7826e723
kanon chakma <kanonchakma@pop-os.localdomain>
2022-12-16 13:04:16 +0600

    feat: add userimage

---
+++ client/src/component/UserImage.tsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+interface imageProps {
+  image: string;
+  size: string;
+}
+
+const UserImage: React.FC<imageProps> = ({ image, size = "60px" }) => {
+  return (
+    <Box width={size} height={size}>
+      <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width={size}
+        height={size}
+        alt="user"
+        src={`http://localhost:3001/assets/${image}`}
+      />
+    </Box>
+  );
+};
+export default UserImage;
